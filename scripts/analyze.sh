52b0755556e932e1524a9caf52e4373c63374ef3
Tavian Barnes <tavianator@tavianator.com>
2021-02-19 16:14:31 -0500

    scripts/analyze: Import DWARF info
    Summary:
    create-table.py needs DWARF info to find data structures, so fix up the
    scripts to extract it automatically.
    
    Test Plan: ./scripts/analyze.sh factor
    
    Reviewers: ali
    
    Reviewed By: ali
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D425

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -8,14 +8,12 @@ EOF
 fi
 
 ROOT=$(cd "$(dirname "$0")"/.. && pwd)
-BINARY=$1
-BIN=$2
+FULL=$1
+SHORT=$(basename "$FULL")
+HEADLESS=/usr/local/share/ghidra/support/analyzeHeadless
 
 mkdir -p "$ROOT/ghidra-projects"
 
-headless() {
-    /usr/local/share/ghidra/support/analyzeHeadless "$ROOT/ghidra-projects" bcpi -import "$BINARY" -readOnly -scriptPath "$ROOT" -postScript "$@"
-}
-
-headless scripts/create-table.py "$ROOT/data/pickle.pkl"
-headless scripts/analysis-cachemiss.py "$ROOT/data/address_info.csv" "$ROOT/data/pickle.pkl"
+$HEADLESS "$ROOT/ghidra-projects" "$SHORT" -import "$FULL" -postScript DWARF_ExtractorScript.java
+$HEADLESS "$ROOT/ghidra-projects" "$SHORT" -process "$SHORT" -noanalysis -scriptPath "$ROOT/scripts" -postScript create-table.py "$ROOT/data/$SHORT.pkl"
+$HEADLESS "$ROOT/ghidra-projects" "$SHORT" -process "$SHORT" -noanalysis -scriptPath "$ROOT/scripts" -postScript analysis-cachemiss.py "$ROOT/data/address_info.csv" "$ROOT/data/$SHORT.pkl"
