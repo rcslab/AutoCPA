rcslab/AutoCPA

commit d6cdd3a8e21f27e70538850673f5353de0712bdc

analysis: Track accessed byte ranges precisely
Summary:
Rather than data flow analysis returning the set of accessed fields for
every memory operation, it now returns the exact byte range of the
access.  The main benefit of this change is that we track the specific
fields accessed inside nested structs rather than counting the entire
nested structure.

Test Plan: for csv in cpu2017/*.csv; ./scripts/analyze.sh "$csv" "${csv%.csv}"

Reviewers: ali, oscar

Reviewed By: ali

Differential Revision: https://review.rcs.uwaterloo.ca/D701

scripts/bcpi/CacheCostModel.java
@@ -3,6 +3,7 @@ package bcpi;
 import ghidra.program.model.data.DataTypeComponent;
 import ghidra.program.model.data.Structure;
 
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.List;
 import java.util.Map;
@@ -40,23 +41,35 @@ public class CacheCostModel implements CostModel<Structure> {
 	 * Compute a fast mapping between the original and optimized fields, since the
 	 * access patterns refer to the original fields.
 	 */
-	private Field[] getFieldPermutation(List<Field> fields) {
+	private int[] getFieldPermutation(List<Field> fields) {
 		Map<String, Field> fieldMap = fields
 			.stream()
 			.collect(Collectors.toMap(f -> f.getFieldName(), f -> f));
 
-		Field[] fieldPerm = new Field[this.original.getNumComponents()];
+		int[] bytePerm = new int[this.original.getLength()];
+		Arrays.fill(bytePerm, -1);
+
 		for (Field origField : this.originalFields) {
 			Field optField = fieldMap.get(origField.getFieldName());
-			fieldPerm[origField.getOrdinal()] = optField;
+			if (optField == null) {
+				continue;
+			}
+
+			int start = origField.getOffset();
+			int end = Math.min(origField.getEndOffset(), bytePerm.length);
+			int delta = optField.getOffset() - start;
+			for (int i = start; i < end; ++i) {
+				bytePerm[i] = i + delta;
+			}
 		}
-		return fieldPerm;
+
+		return bytePerm;
 	}
 
 	@Override
 	public long cost(Structure struct) {
 		List<Field> fields = Field.allFields(struct);
-		Field[] fieldPerm = getFieldPermutation(fields);
+		int[] bytePerm = getFieldPermutation(fields);
 
 		// As a heuristic, assume offset zero is twice as likely as the next possible
 		// offset, which is twice as likely as the next one, etc.
@@ -78,16 +91,11 @@ public class CacheCostModel implements CostModel<Structure> {
 			for (AccessPattern pattern : this.patterns.getRankedPatterns(this.original)) {
 				long count = this.patterns.getCount(pattern);
 
-				for (Field origField : pattern.getFields()) {
-					Field optField = fieldPerm[origField.getOrdinal()];
-					if (optField == null) {
-						continue;
-					}
-
-					int start = (offset + optField.getOffset()) / CACHE_LINE;
-					int end = (offset + optField.getEndOffset() - 1) / CACHE_LINE;
-					touchedLines.set(start, end + 1);
-				}
+				pattern.getBytes()
+					.stream()
+					.map(i -> bytePerm[i])
+					.filter(i -> i >= 0)
+					.forEach(i -> touchedLines.set(i / CACHE_LINE));
 
 				cost += count * touchedLines.cardinality();
 

scripts/StructOrderAnalysis.java
@@ -514,7 +514,7 @@ public class StructOrderAnalysis extends GhidraScript {
 			out.println("    max-height: 100vh;");
 			out.println("    overflow-y: scroll;");
 			out.println("}");
-			out.println(".pattern ul {");
+			out.println(".pattern ul.functions {");
 			out.println("    columns: 2;");
 			out.println("    column-fill: auto;");
 			out.println("    max-height: 120px;");
@@ -702,8 +702,8 @@ public class StructOrderAnalysis extends GhidraScript {
 			}
 
 			for (Field field : pattern.getFields()) {
-				boolean read = pattern.getReadFields().contains(field);
-				boolean written = pattern.getWrittenFields().contains(field);
+				boolean read = pattern.reads(field);
+				boolean written = pattern.writes(field);
 
 				for (DataTypeComponent component : field.getComponents()) {
 					String name = component.getFieldName();
@@ -902,17 +902,48 @@ public class StructOrderAnalysis extends GhidraScript {
 
 			long count = patterns.getCount(pattern);
 			int percentage = percent(count, total);
-			out.format("%d%% (%,d times)<br><code>%s</code>\n", percentage, count, pattern);
+			out.format("%d%% (%,d times)<br>\n", percentage, count);
 
-			out.println("<ul>");
+			renderAccessPattern(pattern, "", out);
+
+			out.println("Occurs in");
+			out.println("<ul class='functions'>");
 			patterns.getFunctions(pattern)
 				.stream()
 				.map(f -> f.getName())
 				.sorted()
+				.map(f -> htmlEscape(f))
 				.forEach(f -> out.format("<li><code>%s()</code>\n", f));
 			out.println("</ul>");
 
 			++id;
+		}
+
+		out.println("</ul>");
+	}
+
+	private void renderAccessPattern(AccessPattern pattern, String fieldName, PrintWriter out) {
+		String decl = DataTypes.formatCDecl(pattern.getType(), fieldName);
+
+		out.println("<code>" + htmlEscape(decl) + "</code>");
+		out.println("<ul>");
+
+		for (DataTypeComponent field : pattern.getType().getDefinedComponents()) {
+			if (!pattern.accesses(field)) {
+				continue;
+			}
+
+			out.print("<li>");
+
+			AccessPattern proj = pattern.project(field);
+			if (proj == null) {
+				String r = pattern.reads(field) ? "R" : "";
+				String w = pattern.writes(field) ? "W" : "";
+				String fieldDecl = DataTypes.formatCDecl(field.getDataType(), field.getFieldName());
+				out.format("<code>%s <strong>(%s%s)</strong></code>\n", htmlEscape(fieldDecl), r, w);
+			} else {
+				renderAccessPattern(proj, field.getFieldName(), out);
+			}
 		}
 
 		out.println("</ul>");

scripts/bcpi/AccessPattern.java
@@ -1,64 +1,171 @@
 package bcpi;
 
 import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.Composite;
+import ghidra.program.model.data.DataTypeComponent;
+import ghidra.program.model.data.Structure;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
-
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * A set of fields accessed in a block.
  */
 public class AccessPattern {
-	private final Set<Field> read;
-	private final Set<Field> written;
+	private final Composite type;
+	private final BitSet read;
+	private final BitSet written;
 
-	public AccessPattern(Set<Field> read, Set<Field> written) {
-		this.read = ImmutableSet.copyOf(read);
-		this.written = ImmutableSet.copyOf(written);
+	public AccessPattern(Composite type, BitSet read, BitSet written) {
+		this.type = type;
+		this.read = (BitSet) read.clone();
+		this.written = (BitSet) written.clone();
 	}
 
-	public Set<Field> getFields() {
-		return Sets.union(this.read, this.written);
+	/**
+	 * @return The type this access pattern applies to.
+	 */
+	public Composite getType() {
+		return this.type;
 	}
 
-	public Set<Field> getReadFields() {
-		return this.read;
+	/**
+	 * @return All the bytes accessed by this pattern.
+	 */
+	public BitSet getBytes() {
+		BitSet bytes = getReadBytes();
+		bytes.or(this.written);
+		return bytes;
 	}
 
-	public Set<Field> getWrittenFields() {
-		return this.written;
+	/**
+	 * @return The bytes read by this pattern.
+	 */
+	public BitSet getReadBytes() {
+		return (BitSet) this.read.clone();
+	}
+
+	/**
+	 * @return The bytes written by this pattern.
+	 */
+	public BitSet getWrittenBytes() {
+		return (BitSet) this.written.clone();
+	}
+
+	/**
+	 * @return All the fields accessed by this pattern.
+	 */
+	public List<Field> getFields() {
+		return Field.allFields((Structure) this.type)
+			.stream()
+			.filter(this::accesses)
+			.collect(Collectors.toList());
+	}
+
+	/**
+	 * @return Whether a field is accessed by this pattern.
+	 */
+	public boolean accesses(Field field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset());
+		return this.read.intersects(bytes) || this.written.intersects(bytes);
+	}
+
+	/**
+	 * @return Whether a field is accessed by this pattern.
+	 */
+	public boolean accesses(DataTypeComponent field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset() + 1);
+		return this.read.intersects(bytes) || this.written.intersects(bytes);
+	}
+
+	/**
+	 * @return Whether a field is read by this pattern.
+	 */
+	public boolean reads(Field field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset());
+		return this.read.intersects(bytes);
+	}
+
+	/**
+	 * @return Whether a field is read by this pattern.
+	 */
+	public boolean reads(DataTypeComponent field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset() + 1);
+		return this.read.intersects(bytes);
+	}
+
+	/**
+	 * @return Whether a field is written by this pattern.
+	 */
+	public boolean writes(Field field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset());
+		return this.written.intersects(bytes);
+	}
+
+	/**
+	 * @return Whether a field is written by this pattern.
+	 */
+	public boolean writes(DataTypeComponent field) {
+		BitSet bytes = new BitSet();
+		bytes.set(field.getOffset(), field.getEndOffset() + 1);
+		return this.written.intersects(bytes);
+	}
+
+	/**
+	 * @return The portion of this access pattern that applies to a field,
+	 *         or null if the field is not a composite.
+	 */
+	public AccessPattern project(DataTypeComponent field) {
+		DataType type = field.getDataType();
+		type = DataTypes.resolve(type);
+		if (type instanceof Composite) {
+			type = DataTypes.dedup(type);
+			BitSet read = this.read.get(field.getOffset(), field.getEndOffset() + 1);
+			BitSet written = this.read.get(field.getOffset(), field.getEndOffset() + 1);
+			return new AccessPattern((Composite) type, read, written);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder result = new StringBuilder();
+		return toString(DataTypes.formatCDecl(this.type) + "::");
+	}
 
-		DataType struct = null;
-		for (Field field : this.getFields()) {
-			if (struct == null) {
-				struct = field.getParent();
-				result.append(struct.getName())
-					.append("::{");
+	private String toString(String prefix) {
+		List<String> fields = new ArrayList<>();
+
+		for (DataTypeComponent field : this.type.getDefinedComponents()) {
+			if (!accesses(field)) {
+				continue;
+			}
+
+			AccessPattern proj = project(field);
+			if (proj == null) {
+				String r = reads(field) ? "R" : "";
+				String w = writes(field) ? "W" : "";
+				fields.add(String.format("%s(%s%s)", field.getFieldName(), r, w));
 			} else {
-				result.append(", ");
+				fields.add(proj.toString(field.getFieldName() + "."));
 			}
-			result.append(field.getFieldName())
-				.append("(");
-			if (this.read.contains(field)) {
-				result.append("R");
-			}
-			if (this.written.contains(field)) {
-				result.append("W");
-			}
-			result.append(")");
 		}
 
-		return result
-			.append("}")
-			.toString();
+		if (fields.size() == 1) {
+			return prefix + fields.get(0);
+		} else {
+			return fields
+				.stream()
+				.collect(Collectors.joining(", ", prefix + "{", "}"));
+		}
 	}
 
 	@Override
@@ -70,12 +177,13 @@ public class AccessPattern {
 		}
 
 		AccessPattern other = (AccessPattern) obj;
-		return this.read.equals(other.read)
+		return this.type.equals(other.type)
+			&& this.read.equals(other.read)
 			&& this.written.equals(other.written);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.read, this.written);
+		return Objects.hash(this.type, this.read, this.written);
 	}
 }

scripts/bcpi/AccessPatterns.java
@@ -6,14 +6,15 @@ import ghidra.program.model.data.Structure;
 import ghidra.program.model.listing.Function;
 import ghidra.util.Msg;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.SetMultimap;
 import com.google.common.collect.Sets;
 
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -55,8 +56,8 @@ public class AccessPatterns {
 			return;
 		}
 
-		SetMultimap<Structure, Field> reads = HashMultimap.create();
-		SetMultimap<Structure, Field> writes = HashMultimap.create();
+		Map<Structure, BitSet> reads = new HashMap<>();
+		Map<Structure, BitSet> writes = new HashMap<>();
 
 		Set<CodeBlock> blocks = getCodeBlocksThrough(row.function, row.address);
 		for (CodeBlock block : blocks) {
@@ -67,9 +68,11 @@ public class AccessPatterns {
 				}
 
 				for (FieldReference ref : this.refs.getFields(address)) {
-					Field field = ref.getField();
-					Structure struct = field.getParent();
-					(ref.isRead() ? reads : writes).put(struct, field);
+					for (FieldReference structRef : ref.getStructRefs()) {
+						(structRef.isRead() ? reads : writes)
+							.computeIfAbsent((Structure) structRef.getOuterType(), k -> new BitSet())
+							.set(structRef.getOffset(), structRef.getEndOffset());
+					}
 				}
 			}
 		}
@@ -80,7 +83,9 @@ public class AccessPatterns {
 		}
 
 		for (Structure struct : Sets.union(reads.keySet(), writes.keySet())) {
-			AccessPattern pattern = new AccessPattern(reads.get(struct), writes.get(struct));
+			BitSet read = reads.getOrDefault(struct, new BitSet());
+			BitSet written = writes.getOrDefault(struct, new BitSet());
+			AccessPattern pattern = new AccessPattern(struct, read, written);
 			this.patterns
 				.computeIfAbsent(struct, k -> ConcurrentHashMap.newKeySet())
 				.add(pattern);

scripts/bcpi/FieldReference.java
@@ -1,34 +1,110 @@
 package bcpi;
 
+import ghidra.program.model.data.Composite;
+import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.DataTypeComponent;
+import ghidra.program.model.data.Structure;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Metadata about a struct field reference.
  */
 public class FieldReference {
-	private final Field field;
-	private final boolean arrayAccess;
-	private final boolean isRead;
+	private final Composite outerType;
+	private final boolean outerArray;
+	private final int offset;
+	private final int size;
+	private final boolean read;
 
-	FieldReference(Field field, boolean arrayAccess, boolean isRead) {
-		this.field = field;
-		this.arrayAccess = arrayAccess;
-		this.isRead = isRead;
+	FieldReference(Composite outerType, boolean outerArray, int offset, int size, boolean read) {
+		Objects.checkFromIndexSize(offset, size, outerType.getLength());
+		this.outerType = outerType;
+		this.outerArray = outerArray;
+		this.offset = offset;
+		this.size = size;
+		this.read = read;
 	}
 
-	/** The field being accessed. */
-	public Field getField() {
-		return this.field;
+	/**
+	 * @return The type of the outermost object whose field is being accessed.
+	 */
+	public Composite getOuterType() {
+		return this.outerType;
 	}
 
-	/** Whether this field was accessed as part of an array. */
-	public boolean isArrayAccess() {
-		return this.arrayAccess;
+	/**
+	 * @return Whether the outermost object is part of an array.
+	 */
+	public boolean isOuterArray() {
+		return this.outerArray;
 	}
 
-	/** Whether this access was a read access. */
+	/**
+	 * @return The offset of the memory access into the outer object.
+	 */
+	public int getOffset() {
+		return this.offset;
+	}
+
+	/**
+	 * @return The size of this memory access in bytes.
+	 */
+	public int getSize() {
+		return this.size;
+	}
+
+	/**
+	 * @return The (exclusive) end offset of the memory access into the outer object.
+	 */
+	public int getEndOffset() {
+		return this.offset + this.size;
+	}
+
+	/**
+	 * @return Whether this access was a read access.
+	 */
 	public boolean isRead() {
-		return this.isRead;
+		return this.read;
+	}
+
+	/**
+	 * @return All (sub)structures referenced by this access.
+	 */
+	public List<FieldReference> getStructRefs() {
+		List<FieldReference> refs = new ArrayList<>();
+		collectStructRefs(refs);
+		return refs;
+	}
+
+	private void collectStructRefs(List<FieldReference> refs) {
+		if (!(this.outerType instanceof Structure)) {
+			// TODO: Handle unions
+			return;
+		}
+
+		refs.add(this);
+
+		for (DataTypeComponent field : this.outerType.getDefinedComponents()) {
+			DataType type = field.getDataType();
+			type = DataTypes.resolve(type);
+			if (!(type instanceof Composite)) {
+				continue;
+			}
+
+			// Clip the memory access to the bounds of the field
+			int offset = field.getOffset();
+			int start = Math.max(getOffset(), field.getOffset());
+			int end = Math.min(getEndOffset(), field.getEndOffset() + 1);
+
+			if (start < end) {
+				Composite outerType = (Composite) DataTypes.dedup(type);
+				new FieldReference(outerType, this.outerArray, start - offset, end - start, this.read)
+					.collectStructRefs(refs);
+			}
+		}
 	}
 
 	@Override
@@ -40,22 +116,23 @@ public class FieldReference {
 		}
 
 		FieldReference other = (FieldReference) obj;
-		return this.field.equals(other.field)
-			&& this.arrayAccess == other.arrayAccess
-			&& this.isRead == other.isRead;
+		return this.outerType.equals(other.outerType)
+			&& this.outerArray == other.outerArray
+			&& this.offset == other.offset
+			&& this.size == other.size
+			&& this.read == other.read;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.field, this.arrayAccess, this.isRead);
+		return Objects.hash(this.outerType, this.outerArray, this.offset, this.size, this.read);
 	}
 
 	@Override
 	public String toString() {
-		String parent = this.field.getParent().getName();
-		String array = this.arrayAccess ? "[]" : "";
-		String name = this.field.getFieldName();
-		String rw = this.isRead ? "R" : "W";
-		return String.format("%s%s::%s(%s)", parent, array, name, rw);
+		String type = DataTypes.formatCDecl(this.outerType);
+		String array = this.outerArray ? "[]" : "";
+		String rw = this.read ? "R" : "W";
+		return String.format("(%s%s + %d)%s(%d)", type, array, this.offset, rw, this.size);
 	}
 }

scripts/bcpi/FieldReferences.java
@@ -2,7 +2,7 @@ package bcpi;
 
 import ghidra.program.model.address.Address;
 import ghidra.program.model.block.CodeBlock;
-import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.Composite;
 import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.FunctionManager;
 import ghidra.program.model.pcode.HighFunction;
@@ -166,14 +166,15 @@ public class FieldReferences {
 			return;
 		}
 
-		DataType type = facts.getType();
+		Composite type = facts.getType();
 		int offset = facts.getOffset().getAsInt();
-		int endOffset = offset + value.getSize();
+		int size = value.getSize();
 
-		Set<FieldReference> fields = updateFields(op.getSeqnum().getTarget());
-		DataTypes.getFieldsBetween(type, offset, endOffset)
-			.stream()
-			.map(f -> new FieldReference(f, facts.isArray(), isRead))
-			.forEach(fields::add);
+		if (offset < 0 || offset + size > type.getLength()) {
+			return;
+		}
+
+		updateFields(op.getSeqnum().getTarget())
+			.add(new FieldReference(type, facts.isArray(), offset, size, isRead));
 	}
 }

commit 5775d0c455ecde96c1fef10d0f1fce13bf4d014e

analysis: Use the cache line offset correctly
Summary:
Previously the cost model was not using the cache line offset when
mapping memory accesses to the affected cache lines.

Fixes: d6cdd3a8e

Test Plan: ./scripts/analyze.sh cpu2017/mcf_r_base.mytest-m64{.csv,}

Reviewers: ali, oscar, jiafengyu, ary

Reviewed By: ali

Differential Revision: https://review.rcs.uwaterloo.ca/D794

scripts/bcpi/CacheCostModel.java
@@ -81,7 +81,9 @@ public class CacheCostModel implements CostModel<Structure> {
 		// Compute the expected cost over the possible cache line offsets
 		long total = 0;
 		int weightShift = nOffsets;
-		for (int offset = 0; offset < CACHE_LINE; offset += this.align) {
+		for (int i = 0; i < nOffsets; ++i) {
+			int offset = i * this.align;
+
 			// For each offset, the cost is the cumulative number of cache lines touched
 			// up to the current pattern, weighted by the pattern's observation count.
 			// You can think of this like the area under the (pattern, cache lines) curve.
@@ -93,9 +95,9 @@ public class CacheCostModel implements CostModel<Structure> {
 
 				pattern.getBytes()
 					.stream()
-					.map(i -> bytePerm[i])
-					.filter(i -> i >= 0)
-					.forEach(i -> touchedLines.set(i / CACHE_LINE));
+					.map(j -> bytePerm[j])
+					.filter(j -> j >= 0)
+					.forEach(j -> touchedLines.set((offset + j) / CACHE_LINE));
 
 				cost += count * touchedLines.cardinality();
 